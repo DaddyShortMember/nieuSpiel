4e72932a8a275613cc8007c1a018ee0ff8e80b41
Mario E. Perez F <90684949+DaddyShortMember@users.noreply.github.com>
2021-10-24 22:25:39 +0200

    Delete src/jClasses directory

--- src/jClasses/Asche.java
+++
@@ -1,9 +0,0 @@
-package jClasses;
-
-public class Asche {
-public static void main(String[] args) {
-	Rifleman lolmao = new Rifleman();
-	
-	System.out.println(lolmao);
-}
-}
--- src/jClasses/Vehiculo.java
+++
@@ -1,5 +0,0 @@
-package jClasses;
-
-public abstract class Vehiculo extends Tropa {
-
-}
