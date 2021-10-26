20cb8892c03f3332073ce458ea5fb9edab5e7307
Mario E. Perez F <90684949+DaddyShortMember@users.noreply.github.com>
2021-10-26 23:33:38 +0200

    Update ListaArmas.java

--- src/jClasses/ListaArmas.java
+++ src/jClasses/ListaArmas.java
@@ -7,7 +7,7 @@ public enum ListaArmas {
 	BOMBA,
 	COHETE,
 	TORPEDO,
-	MISIL,
+	MISIL,	
 	MISIL_AA, //Anti-aereos
 	MISIL_AB, //Anti-barcos
 	ARMA_AA, //Arma anti-aerea 
@@ -16,4 +16,6 @@ public enum ListaArmas {
 	TCANNON_LETAL, //Cannons de tanques. Es el mas pesado.
 	TCANNON_MED, //Cannons de tanques. Intermedio
 	TCANNON 
+	
+	//Si hace falta alguna, add it.
 }
