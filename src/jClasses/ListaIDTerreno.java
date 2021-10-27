1663770d2f1b6277fadcb31042f039560c9bd3a8
Mario E. Perez F <90684949+DaddyShortMember@users.noreply.github.com>
2021-10-27 17:46:01 +0200

    Update ListaIDTerreno.java

--- src/jClasses/ListaIDTerreno.java
+++ src/jClasses/ListaIDTerreno.java
@@ -2,26 +2,27 @@ package jClasses;
 
 public enum ListaIDTerreno {
 
-//TERRENO INDOMABLE
-		PLAINS, //Llanos y praderas
-		ROAD, //Carreteras
-		BRIDGE, //Puentes? En serio, Rodrigo?
-		MOUNTAIN, //Montanas y montes altos
-		SEA, //El mar azul tan bello
-		ARCHIPELAGO, //Conjunto de islas cercanas
-		COAST, //Costas, donde se pueden construir
-		FOREST, //Bosques
-		//ESTRUCTURAS CAPTURABLES
-		HQ, //Cuarteles generales
-		CITY, //Ciudad
-		FACTORY, //Fabricas
-		AIRPORT, //Aeropuerto
-		PORT, //Puerto
-		//Construibles
-		TEMP_SEAP, //Puerto Temporal
-		TEMP_AIRP //Aeropuerto Temporal, 
+	//TERRENO INDOMABLE
+	PLAINS, //Llanos y praderas
+	ROAD, //Carreteras
+	BRIDGE, //Puentes? En serio, Rodrigo?
+	MOUNTAIN, //Montanas y montes altos
+	SEA, //El mar azul tan bello
+	ARCHIPELAGO, //Conjunto de islas cercanas
+	COAST, //Costas, donde se pueden construir
+	FOREST, //Bosques
+	
+	//ESTRUCTURAS CAPTURABLES
+	HQ, //Cuarteles generales
+	CITY, //Ciudad
+	FACTORY, //Fabricas
+	AIRPORT, //Aeropuerto
+	PORT, //Puerto
+		
+	//ESTRUCTURAS CONSTRUIBLES
+	TEMP_SEAP, //Puerto Temporal
+	TEMP_AIRP //Aeropuerto Temporal, 
 
-//ESTRUCTURA
 
 
 }
