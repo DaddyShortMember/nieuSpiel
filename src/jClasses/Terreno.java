c92d3798ff0ab14c3ce1dcb71a9ece9d89217cb8
Mario E. Perez F <90684949+DaddyShortMember@users.noreply.github.com>
2021-10-27 17:24:42 +0200

    Update Terreno.java

--- src/jClasses/Terreno.java
+++ src/jClasses/Terreno.java
@@ -2,89 +2,37 @@ package jClasses;
 
 public abstract class Terreno {
 	
-	enum IdentidadTerreno{
-		ALGO,
-		
+		enum IdentidadTerreno{
+		//TERRENO INDOMABLE
+		PLAINS, //Llanos y praderas
+		ROAD, //Carreteras
+		BRIDGE, //Puentes? En serio, Rodrigo?
+		MOUNTAIN, //Montanas y montes altos
+		SEA, //El mar azul tan bello
+		ARCHIPELAGO, //Conjunto de islas cercanas
+		COAST, //Costas, donde se pueden construir
+		FOREST, //Bosques
+		//ESTRUCTURAS CAPTURABLES
+		HQ, //Cuarteles generales
+		CITY, //Ciudad
+		FACTORY, //Fabricas
+		AIRPORT, //Aeropuerto
+		PORT, //Puerto
+		//Construibles
+		TEMP_SEAP, //Puerto Temporal
+		TEMP_AIRP //Aeropuerto Temporal, 
 	}
 	
 	
-	
+	IdentidadTerreno idTerreno; //Identidad del terreno.
 	String nombre; //Nombre del terreno
-	int peaje; //Lo que cuesta pasar por este terreno
+	boolean construible; //Si se puede construir o no; usado por aeropuerto y puerto temporal
+	boolean construibleSobre; //Si se puede construir sobre este terreno. Implementado por cada grano de tierra y mar.
+	int peaje; //Lo que cuesta pasar por este terreno. Depende del tipo de tropa, por lo que se debe implementar arlgun metodo.
 	int defensa; //Influye en el golpe total que recibe cada tropa. Mientras mas defensa, menos golpe recibido.
 	//Datos Fisicos
 	String imagen;
 	int posX;
 	int posY;
-	
-	public Terreno(String nombre, int peaje, int defensa, String imagen, int posX, int posY) {
-		super();
-		this.nombre = nombre;
-		this.peaje = peaje;
-		this.defensa = defensa;
-		this.imagen = imagen;
-		this.posX = posX;
-		this.posY = posY;
-	}
-	
-	public Terreno() {
-		this.nombre = "";
-		this.peaje = 0;
-		this.defensa = 0;
-		this.imagen = "";
-		this.posX = 0;
-		this.posY = 0;
-	}
-
-	public String getNombre() {
-		return nombre;
-	}
-
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-
-	public int getPeaje() {
-		return peaje;
-	}
-
-	public void setPeaje(int peaje) {
-		this.peaje = peaje;
-	}
-
-	public int getDefensa() {
-		return defensa;
-	}
-
-	public void setDefensa(int defensa) {
-		this.defensa = defensa;
-	}
-
-	public String getImagen() {
-		return imagen;
-	}
-
-	public void setImagen(String imagen) {
-		this.imagen = imagen;
-	}
-
-	public int getPosX() {
-		return posX;
-	}
-
-	public void setPosX(int posX) {
-		this.posX = posX;
-	}
-
-	public int getPosY() {
-		return posY;
-	}
-
-	public void setPosY(int posY) {
-		this.posY = posY;
-	}
-	
-	
-	
-	
+	//Imprescindible realizar constructores otra vez
 }
