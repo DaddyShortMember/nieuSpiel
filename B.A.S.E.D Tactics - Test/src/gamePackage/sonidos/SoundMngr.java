9729ef4fd9787b4d25ff30f04df672ebce3d946e
Mario E. Perez F <90684949+DaddyShortMember@users.noreply.github.com>
2021-12-29 16:37:34 +0100

    Experimental Update on Sound Manager
    It's a step forward, but it doesn't work.

--- B.A.S.E.D Tactics - Test/src/gamePackage/sonidos/SoundMngr.java
+++ B.A.S.E.D Tactics - Test/src/gamePackage/sonidos/SoundMngr.java
@@ -1,7 +1,11 @@
 package gamePackage.sonidos;
 
+import java.net.MalformedURLException;
+import java.net.URI;
 import java.net.URL; //Para directorios; ej. "fx/sonido1.wav"
 import javax.sound.sampled.*; //Librerias que permiten utilizar audio (limitado a .wav y otros primitivos)
+import java.io.File;
+import java.io.IOException;
 
 //Gestor de sonido
 /*
@@ -21,5 +25,64 @@ import javax.sound.sampled.*; //Librerias que permiten utilizar audio (limitado 
  * y asi
  */
 public class SoundMngr {
+	private Clip soundbit;
+	public SoundMngr(String fileName) {
+		try {
+		File file = new File(fileName);
+		//if (file.exists()) {
+			AudioInputStream sound = AudioSystem.getAudioInputStream(file);
+			soundbit = AudioSystem.getClip();
+			soundbit.open(sound);
+		//}
+		//else {
+			//System.out.println("file not found in the realm of real files");
+		//} //Credits to Tyler Thomas from that one StackOverflow page for guiding me through these dark times
+		}catch (MalformedURLException e){
+			e.printStackTrace();
+			System.out.println(e);
+		}
+		catch (UnsupportedAudioFileException e){
+			e.printStackTrace();
+			System.out.println(e);
+		}
+		catch (LineUnavailableException e){
+			e.printStackTrace();
+			System.out.println(e);
+		}
+		catch (IOException e){
+			e.printStackTrace();
+			System.out.println(e);
+		}
+		
+		
+	}
+	
+	public void play() {
+		soundbit.setFramePosition(0);
+		soundbit.start();
+	}
+	
+	public void loop() {
+		soundbit.loop(Clip.LOOP_CONTINUOUSLY);
+	}
+	public void stop() {
+		soundbit.stop();
+	}
+	//This might appear useless, but I tend to overengineer things to heck
+	public void playAt(int spot) {
+		soundbit.setFramePosition(spot);
+		soundbit.start();
+	}
+	public void playLoop() {
+		soundbit.setFramePosition(0);
+		soundbit.start();
+		soundbit.loop(Clip.LOOP_CONTINUOUSLY);
+	}
+	
+	public static void main(String[] args) {
 
+		SoundMngr x = new SoundMngr("fx/CantinaBand60.wav");
+		x.play();
+		x.loop();
+	}
 }
