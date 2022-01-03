6f8ba00953b42e97805ae171ebbf1284f81f1e84
Mario E. Perez F <90684949+DaddyShortMember@users.noreply.github.com>
2022-01-03 17:37:05 +0100

    Add files via upload

--- B.A.S.E.D Tactics - Test/src/gamePackage/sonidos/SoundMngr.java
+++ B.A.S.E.D Tactics - Test/src/gamePackage/sonidos/SoundMngr.java
@@ -1,19 +1,19 @@
 package gamePackage.sonidos;
 
 import java.net.MalformedURLException;
-import java.net.URI;
 import java.net.URL; //Para directorios; ej. "fx/sonido1.wav"
 import javax.sound.sampled.*; //Librerias que permiten utilizar audio (limitado a .wav y otros primitivos)
-import java.io.File;
+
+import com.sun.tools.javac.Main; //Util para obtener directorio
+
+import java.io.File; //Fichero
 import java.io.IOException;
-//TODO: !!!AUN NO FUNCIONA!!!
-//Da error por que no reconoce el URL como directorio.
+import java.lang.System.Logger;
+import java.lang.System.Logger.Level;
+
 //Gestor de sonido
-/*
- * Idea: crea un hilo por sonido para permitir el uso de multiples sonidos a la vez
- * Abierto a experimentacion
- * 
- * Idea para gestion de directorios: ya que son dos musicas distintas por cada turno,
+
+/* Idea para gestion de directorios: ya que son dos musicas distintas por cada turno,
  * se puede hacer que cada turno cambie el audio.
  * 
  * int rndA //Uno por cada equipo
@@ -25,65 +25,59 @@ import java.io.IOException;
  * 
  * y asi
  */
-public class SoundMngr {
-	private Clip soundbit;
-	public SoundMngr(String fileName) {
-		try {
-		File file = new File(fileName);
-		//if (file.exists()) {
-			AudioInputStream sound = AudioSystem.getAudioInputStream(file);
-			soundbit = AudioSystem.getClip();
-			soundbit.open(sound);
-		//}
-		//else {
-			//System.out.println("file not found in the realm of real files");
-		//} //Credits to Tyler Thomas from that one StackOverflow page for guiding me through these dark times
-		}catch (MalformedURLException e){
-			e.printStackTrace();
-			System.out.println(e);
-		}
-		catch (UnsupportedAudioFileException e){
-			e.printStackTrace();
-			System.out.println(e);
-		}
-		catch (LineUnavailableException e){
-			e.printStackTrace();
-			System.out.println(e);
-		}
-		catch (IOException e){
-			e.printStackTrace();
-			System.out.println(e);
-		}
-		
-		
-	}
-	
-	public void play() {
-		soundbit.setFramePosition(0);
-		soundbit.start();
-	}
-	
-	public void loop() {
-		soundbit.loop(Clip.LOOP_CONTINUOUSLY);
-	}
-	public void stop() {
-		soundbit.stop();
-	}
-	//This might appear useless, but I tend to overengineer things to heck
-	public void playAt(int spot) {
-		soundbit.setFramePosition(spot);
-		soundbit.start();
-	}
-	public void playLoop() {
-		soundbit.setFramePosition(0);
-		soundbit.start();
-		soundbit.loop(Clip.LOOP_CONTINUOUSLY);
-	}
-	
-	public static void main(String[] args) {
 
-		SoundMngr x = new SoundMngr("fx/CantinaBand60.wav");
-		x.play();
-		x.loop();
+//Credits to the Java Sound API crew and 
+public class SoundMngr {
+	Object currentSound;
+	String currentName;
+	int gain;
+	long duration;
+	Mixer mixer;
+	Clip clip;
+	File file;
+	Logger log;
+	
+
+	public SoundMngr(Object name, int rule, int vol) {
+		if (name != null) {
+			URL url = SoundMngr.class.getResource("/gamePackage/sonidos/fx/" + (String) name);
+			if (url != null) {
+				try {
+					AudioInputStream audioStream = AudioSystem.getAudioInputStream(url);
+					DataLine.Info dataInfo = new DataLine.Info(Clip.class, audioStream.getFormat());
+					clip = (Clip) AudioSystem.getLine(dataInfo);
+					duration = audioStream.getFrameLength();
+					clip.open(audioStream);
+					clip.start();
+					if (rule == 0) {
+						clip.loop(Clip.LOOP_CONTINUOUSLY);
+						Thread.sleep(duration);
+					}else {
+						Thread.sleep(duration);
+						clip.close();
+						audioStream.close();
+					}
+				} catch (UnsupportedAudioFileException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (LineUnavailableException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		} else
+			log.log(Level.INFO, "Unable to read director");
+
+	}
+//Test
+	public static void main(String[] args) {
+		String x = "CantinaBand60.wav";
+		new SoundMngr(x, 0, 100);
 	}
 }
