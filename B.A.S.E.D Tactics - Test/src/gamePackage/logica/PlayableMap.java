3a2f1bf5f768a5feb1dc482dbe3e13e8aa193c6d
PabloDullaghan <90982579+PabloDullaghan@users.noreply.github.com>
2022-01-11 20:43:18 +0100

    Delete PlayableMap.java

--- B.A.S.E.D Tactics - Test/src/gamePackage/logica/PlayableMap.java
+++
@@ -1,9 +0,0 @@
-package gamePackage.logica;
-
-import java.io.Serializable;
-
-public class PlayableMap implements Serializable {
-	//Here go the map deetz, such as arraylists/linkedlists and the likes. This is to create a map selection system of sorts, although extremely barebones.
-
-	//I'm taking a nap now, I'm pretty tired.
-}
